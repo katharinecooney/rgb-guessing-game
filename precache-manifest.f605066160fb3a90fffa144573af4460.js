21134356e8f71de4ff2b0d56e6416ee20b48e50e
Katharine Cooney <katharinecooney@gmail.com>
2019-08-06 16:55:14 -0500

    Updates

---
+++ precache-manifest.f605066160fb3a90fffa144573af4460.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e0aed1e09a320ae9580c1d3863e729",
+    "url": "/rgb-guessing-game/index.html"
+  },
+  {
+    "revision": "23d359e2b2634bbc8087",
+    "url": "/rgb-guessing-game/static/css/main.68bbe2f6.chunk.css"
+  },
+  {
+    "revision": "6d266e28bc6627854b54",
+    "url": "/rgb-guessing-game/static/js/2.d8ac1a3a.chunk.js"
+  },
+  {
+    "revision": "23d359e2b2634bbc8087",
+    "url": "/rgb-guessing-game/static/js/main.2c729bee.chunk.js"
+  },
+  {
+    "revision": "a316798d466407294561",
+    "url": "/rgb-guessing-game/static/js/runtime~main.ea478067.js"
+  }
+]);
