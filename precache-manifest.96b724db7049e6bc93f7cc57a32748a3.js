de2dfd7b89283615fa7fe07f2c9c855d085605c8
Katharine Cooney <katharinecooney@gmail.com>
2019-08-08 18:20:10 -0500

    Updates

--- precache-manifest.96b724db7049e6bc93f7cc57a32748a3.js
+++ precache-manifest.96b724db7049e6bc93f7cc57a32748a3.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6d84d04a9603bafeec626562ff513922",
+    "revision": "4a0d9869c42d7055f3e722b8451fe17b",
     "url": "/rgb-guessing-game/index.html"
   },
   {
-    "revision": "baa35933d0aec1dc416b",
-    "url": "/rgb-guessing-game/static/css/main.68bbe2f6.chunk.css"
+    "revision": "d642c1389bcd55c4dd0b",
+    "url": "/rgb-guessing-game/static/css/main.91a6c134.chunk.css"
   },
   {
     "revision": "6d266e28bc6627854b54",
     "url": "/rgb-guessing-game/static/js/2.d8ac1a3a.chunk.js"
   },
   {
-    "revision": "baa35933d0aec1dc416b",
-    "url": "/rgb-guessing-game/static/js/main.1e4c5044.chunk.js"
+    "revision": "d642c1389bcd55c4dd0b",
+    "url": "/rgb-guessing-game/static/js/main.af203f94.chunk.js"
   },
   {
     "revision": "a316798d466407294561",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rgb-guessing-game/precache-manifest.4f9a8b8a00bda7e5f227c66e6a984220.js"
+  "/rgb-guessing-game/precache-manifest.96b724db7049e6bc93f7cc57a32748a3.js"
 );
 
 self.addEventListener('message', (event) => {
